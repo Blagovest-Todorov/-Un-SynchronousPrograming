749dabc7433940e9cede2734ec18e6a6bff0a3e8
Blagovest Todorov <72020361+Blagovest-Todorov@users.noreply.github.com>
2023-02-15 14:05:42 +0200

    add 3 files explaining diff between sync and async programming
    Syncronous programming, the program is performed row by row with blocking operations, the flow goes on the next line only after the current method or line is executed and does not go down.
    Aync programming, the as soon as there is async Task await,  it is opening a new thread for performing the current line, and the program flow goes on. Async programming helps for fast responding applications and programs.
    https://www.tutorialsteacher.com/articles/asynchronous-programming-with-async-await-task-csharp

---
+++ AsyncFileReturnsValue.cs
@@ -0,0 +1,28 @@
+﻿namespace TestAp.SunCProgram
+{
+    public class AsyncFileReturnsValue
+    {
+        static async Task Main(string[]args) 
+        {
+            Task<int> result = LongProcess();
+            ShortProceass();
+            var val = await result; // wait until get the return value
+            Console.WriteLine("Result: {0}", val);  
+            Console.ReadKey();
+        }
+
+        static async Task<int> LongProcess() 
+        {
+            Console.WriteLine("LongProcess Started"); 
+            await Task.Delay(1000);
+            Console.WriteLine("LongProcess Completed");
+            return 10;
+        }
+        private static void ShortProceass()
+        {
+            Console.WriteLine("ShortProcess Started!");
+            // do sth
+            Console.WriteLine("Shortprocess Completed!");
+        }        
+    }
+}
---
+++ Sync.cs
@@ -0,0 +1,31 @@
+﻿namespace TestAp.SunCProgram
+{
+    public class Sync
+    {
+        //static void Main(string[] args)
+        //{
+        //    LongProcess();
+
+        //    ShortProcess();
+        //}
+
+        //static void LongProcess()
+        //{
+        //    Console.WriteLine("LongProcess Started");
+
+        //    //some code that takes long execution time 
+        //    System.Threading.Thread.Sleep(4000); // hold execution for 4 seconds
+
+        //    Console.WriteLine("LongProcess Completed");
+        //}
+
+        //static void ShortProcess()
+        //{
+        //    Console.WriteLine("ShortProcess Started");
+
+        //    //do something here
+
+        //    Console.WriteLine("ShortProcess Completed");
+        //}
+    }
+}
